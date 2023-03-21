65ba7c071a95826d23b2b8e13c517bca75a35f97
AhmadRaza9 <raza.ataki@gmail.com>
2023-03-21 12:15:15 +0500

    working with event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,18 @@ import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
 
+  const clickHandler = () => {
+    console.log("clickHandler");
+  }
+
   return (
     <Card className="expense-item">
-          <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onMouseOver={clickHandler} onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
